8b62f3fd110254d9945350226b87021d4c9c87e8
Viviana <viviana.tacuri05@gmail.com>
2019-11-20 08:50:03 -0500

    searchRules con reglas y antecedentes

--- api/rules/searchRules.php
+++ api/rules/searchRules.php
@@ -10,41 +10,55 @@
     $objson =json_decode($json);
 
     /*Datos del usuario*/
-   $user = $objson->usuario;
+   //$user = $objson->usuario;
+   $user = 1;
 
    /* */
-
     if($user != null)
     {
         $J=[];
         $I=0;
+        $L=[];
+        $P=0;
 
         $result=$mysqli->query("SELECT ID_REGLA,CONCLUSION FROM reglas WHERE ID_USUARIO='$user'");
-        $fila = $result->fetch_assoc();
-        $id_regla = $fila["ID_REGLA"];
-        $conclusion = $fila["CONCLUSION"];
+        $cont_regla = $result->num_rows;
 
+        while($cont_regla>0){
+          $fila = $result->fetch_assoc();
+          $id_regla = $fila["ID_REGLA"];
+          $conclusion = $fila["CONCLUSION"];
+          
 
-        $antecedente=$mysqli->query("SELECT ID_ANTECEDENTES,DESCRIP_ANT  FROM antecedentes WHERE ID_REGLA='$id_regla'");
-        $cont = $antecedente->num_rows;
+          $antecedente=$mysqli->query("SELECT ID_ANTECEDENTES,DESCRIP_ANT  FROM antecedentes WHERE ID_REGLA='$id_regla'");
+          $cont = $antecedente->num_rows;
 
-        while($cont>0){ //para recorrer los antecedentes
+          $I=0;
+          while($cont>0){ //para recorrer los antecedentes
             $fila_ant = $antecedente->fetch_assoc();
             $descripc_ant = $fila_ant["DESCRIP_ANT"];
             $id_ant = $fila_ant["ID_ANTECEDENTES"];
             
             $J[$I] = [
-                "Id_descripcion"=>$id_ant,
-                "Descripcion"=>$descripc_ant,
+                "id_antec"=>$id_ant,
+                "antecedente"=>$descripc_ant,
             ];
             $I++;
             $cont--;
         }
+          $L[$P]=[
+          "id_regla"=>$id_regla,
+          "conclusion"=>$conclusion,
+          "antecedentes"=>$J
+          ];
+          $P++;
+          $cont_regla--;
+          
+        }
+        
           $H= [
-            "Validacion"=>true,
-            "Id_regla" =>$id_regla,
-            "Conclusion"=>$conclusion,
-            "Antedentes" => $J
+            "validacion"=>true,
+            "regla" => $L
           ];
 
         echo json_encode($H);
