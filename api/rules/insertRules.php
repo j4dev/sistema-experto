af38f3916b43ae4865403df7b39320ff8c896df6
toaquizaesteban <esteban_1.6@hotmail.com>
2019-11-20 01:32:57 -0500

    insertar

--- api/rules/insertRules.php
+++ api/rules/insertRules.php
@@ -24,8 +24,9 @@
         $I=0;
 
         $sql = "INSERT INTO `reglas`(`CONCLUSION`, `ID_USUARIO`) VALUES
-                ('','')";
+                ('$conclusion','$user')";
         $result=$mysqli->query($sql);
+        while()
 
         if($result){
             
