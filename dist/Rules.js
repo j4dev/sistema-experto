da983aecea2404e03a4984df6a41c27b807fd455
j4dev <jesusguanga98@outlook.com>
2019-11-20 03:29:52 -0500

    Metodo addInput funciona correctamente.

--- dist/Rules.js
+++ dist/Rules.js
@@ -2,11 +2,19 @@
 /**
  * Agrear un input al modal de ingreso de reglas
  */
+var inputTotal = 1;
 function addInput() {
+    inputTotal = inputTotal + 1;
     let antece = document.querySelector("#antecedentes");
     let input = document.createElement("INPUT");
+    input.setAttribute("class", "form-control");
+    input.setAttribute("id", inputTotal.toString());
+    input.setAttribute("style", "text-transform:uppercase;");
     antece.appendChild(input);
 }
 function addRule() {
+    var idInput = "#1";
+    var input = document.querySelector(idInput).value.toString();
+    console.log(input);
 }
 //# sourceMappingURL=Rules.js.map
