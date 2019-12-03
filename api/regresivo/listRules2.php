eb66e92d5bada174344f86bf9b6639b536a1fb1c
Viviana <viviana.tacuri05@gmail.com>
2019-12-03 09:59:40 -0500

    list rules 2

---
+++ api/regresivo/listRules2.php
@@ -0,0 +1,50 @@
+<?php
+    require_once('../base_conocimiento/coneccion.php');
+    header('Access-Control-Allow-Origin: *');
+    header("Content-type: application/json");
+    header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+    header('Access-Control-Allow-Methods: POST');
+
+    $J=[];
+    $I=0;
+
+    $json = file_get_contents('php://input');
+    $objson =json_decode($json);
+    $id_regla = $objson->id_regla;
+
+    $conclusionsql=$mysqli->query("SELECT * FROM `reglas` WHERE `ID_REGLA`= '$id_regla'");
+    $fila5 = $conclusionsql->fetch_assoc();
+    $con = $fila5['CONCLUSION'];
+
+    $reglas2=$mysqli->query("SELECT * FROM `reglas` WHERE `ID_REGLA`<$id_regla and `CONCLUSION`='$con' ORDER BY `ID_REGLA` DESC  ");
+    $fila = $reglas2->fetch_assoc();
+    $resp = $fila['ID_REGLA'];
+    
+    if($reglas2 != null){
+                $antecedentes2 = $mysqli->query("SELECT `ID_REGLA`,`DESCRIP_ANT` FROM `antecedentes` WHERE `ID_REGLA` = '$resp'");
+                $cont_ant2 = $antecedentes2->num_rows;
+                while($cont_ant2>0){
+
+                    $fila2 = $antecedentes2->fetch_assoc();
+                    $antecedente2 = $fila2['DESCRIP_ANT'];
+                    $J[$I]=[
+                        "validacion"=>true,
+                        "antecedente"=>$antecedente2
+                    ];
+                    $I++;
+                    $cont_ant2--;
+                }
+    }
+    else{
+        $J[$I]=[
+            "validacion"=>false
+        ];
+
+    }
+
+
+    echo json_encode($J);
+    
+    $mysqli->close();
+
+?>
