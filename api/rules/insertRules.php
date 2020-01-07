9f55caad20cbb3aec3d5fb1338db7e8e107b94ba
j4dev <jesusguanga98@outlook.com>
2020-01-06 21:02:35 -0500

    Insertar

--- api/rules/insertRules.php
+++ api/rules/insertRules.php
@@ -32,27 +32,14 @@
         $last_id = $mysqli->query("SELECT LAST_INSERT_ID() AS id_regla");
         $aux_id = $last_id->fetch_assoc();
         $id_regla = $aux_id['id_regla'];
-<<<<<<< HEAD
+
         $i=0;
         foreach ($tipo as $valor) {
-            $sql = "INSERT INTO `antecedentes`(`ID_REGLA`, `DESCRIP_ANT`,`Ant_porcentaje`) VALUES 
-            ('$id_regla','$valor','$ant_porcentaje[$i]')";
-=======
-
-        
-        foreach ($tipo as $valor) {
             $sql = "INSERT INTO `antecedentes`(`ID_REGLA`, `DESCRIP_ANT`,`Ant_porcentaje`) VALUES
-            ('$id_regla','$valor','$ant_porcentaje')";
-<<<<<<< HEAD
-             $i++;
-
->>>>>>> 05e62ce1c6109410f9330bf56c32ac226a4aa13f
-=======
->>>>>>> 99283307541b2e377fa519c988362e1cb780bb14
+            ('$id_regla','$valor','$ant_porcentaje[$i]')";
             $result=$mysqli->query($sql);
             $i++;
         }
-
         
         if($result){
             
